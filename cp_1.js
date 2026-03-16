4107e4d558f317f692e06cb143fd91e1b03e9c83
tiighe <tigheknowles@gmail.com>
2026-03-15 22:05:20 -0400

    setup javascript file and selected form elements

---
+++ cp_1.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("feedback-form");
+
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const comments = document.getElementById("comments");
+
+const charTotal = document.getElementById("char-total");
+
+const feedbackDisplay = document.getElementById("feedback-display");
+
+const tooltip = document.getElementById("tooltip");
