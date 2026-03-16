c6726c2945e8ad971df59f46c83d79aaa6104ceb
tiighe <tigheknowles@gmail.com>
2026-03-15 22:06:44 -0400

    implemented character counter and tooltip mouse events

--- cp_1.js
+++ cp_1.js
@@ -9,3 +9,37 @@ const charTotal = document.getElementById("char-total");
 const feedbackDisplay = document.getElementById("feedback-display");
 
 const tooltip = document.getElementById("tooltip");
+
+comments.addEventListener("input", function () {
+charTotal.textContent = comments.value.length;
+});
+
+form.addEventListener("mouseover", function (event) {
+
+const target = event.target;
+
+if (target.tagName === "INPUT" || target.tagName === "TEXTAREA") {
+
+tooltip.textContent = "Enter information here";
+
+tooltip.classList.remove("hidden");
+
+tooltip.style.top = event.pageY - 40 + "px";
+
+tooltip.style.left = event.pageX + "px";
+
+}
+
+});
+
+form.addEventListener("mouseout", function (event) {
+
+const target = event.target;
+
+if (target.tagName === "INPUT" || target.tagName === "TEXTAREA") {
+
+tooltip.classList.add("hidden");
+
+}
+
+});
